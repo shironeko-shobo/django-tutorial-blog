1855926ed366c74ba2bc67f99dc71c6b32dfab71
shironeko-shobo <yamaneko623@gmail.com>
2020-07-04 22:22:56 +0900

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#9w&80ve)$&%_1@jfwu+a)s(vun&luqd_j10vx-$29zn9srx#5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
